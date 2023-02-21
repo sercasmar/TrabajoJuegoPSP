bd58af8ff24305624668ed3626227df76740a347
Sergio <74431815+sercasmar@users.noreply.github.com>
2023-02-21 19:18:54 +0100

    Add files via upload

--- src/trabajoprocesos/juego/Servidor.java
+++ src/trabajoprocesos/juego/Servidor.java
@@ -1,6 +1,24 @@
 package trabajoprocesos.juego;
 
+import java.util.ArrayList;
+
 public class Servidor {
+
+    private int idServidor;
+    private ArrayList<Integer> listaPuertos;
+
+    public Servidor() {
+    }
+
+    public void generarPuertos() {
+        listaPuertos = new ArrayList<Integer>();
+        
+        int puertoInicial = idServidor * 10000;
+        
+        for (int i = puertoInicial; i < puertoInicial + 10000; i++) {
+            listaPuertos.add(i);
+        }
+    }
 
     public static void main(String[] args) {
 
